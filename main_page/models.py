85b90a91803fc9bc51a988cdd9ec7e2638a48c7f
Evgeniy <nifpopnaf@gmail.com>
2020-11-03 23:26:39 +0400

    minimize number of sql requests in course_list

--- main_page/models.py
+++ main_page/models.py
@@ -17,9 +17,6 @@ class Course(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=12, default=0.00)
 
-    def student_registered(self, student_id):
-        return self.registrations.filter(student_id=student_id).first()
-
 
 class Lecture(models.Model):
     title = models.CharField(max_length=200)
--- main_page/tasks.py
+++ main_page/tasks.py
@@ -1,9 +1,33 @@
-import time
+
+from datetime import datetime, timedelta
+import django_rq
 from django_rq import job
+from django.contrib.auth.models import User
+from main_page.models import Course, CourseSchedule, CourseRegistration
 
 
-@job("default")
-def send_confirmation_mail(user_mail):
-    print(f'\nsending confirmation mail to {user_mail}')
-    time.sleep(5)
+@job('default')
+def send_confirmation_mail(user_mail=None):
+    print(
+        f'\nconfirmation mail to {user_mail if user_mail else "test@test.ru"}')
     return True
+
+
+@job('low')
+def send_course_begin_mails():
+    print(f'\nsending course warning mails !!!!!!!!')
+    return True
+
+
+IN_24_HOURS = 5  # 24 * 60 * 60
+FOREVER = 3  # None
+
+scheduler = django_rq.get_scheduler(name='low')
+job_low = scheduler.schedule(
+    datetime.utcnow(),
+    send_course_begin_mails,
+    repeat=FOREVER,
+    interval=IN_24_HOURS,
+    result_ttl=600,
+    queue_name='low',
+)
